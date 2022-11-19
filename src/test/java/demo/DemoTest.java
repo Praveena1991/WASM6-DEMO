d0fdb30b0833828d864f3324c4e1d223a5dc589a
PRAVEENA <PRAVEENA@192.168.1.36>
2022-11-19 23:29:45 +0530

    Commit 1 - DemoTest

---
+++ src/test/java/demo/DemoTest.java
@@ -0,0 +1,17 @@
+package demo;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+	@Test
+	public void demo()
+	{
+		System.out.println("This is a demo framework");
+	}
+
+
+}
+  
+
+
+ 
