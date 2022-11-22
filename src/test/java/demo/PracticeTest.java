17aae6700db2096473d4ba2c75d4750a560ae6be
PRAVEENA <PRAVEENA@192.168.1.36>
2022-11-22 19:57:09 +0530

    Branch 1  - TC_Contacts_01 First Commit

---
+++ src/test/java/demo/PracticeTest.java
@@ -0,0 +1,15 @@
+package demo;
+
+import org.testng.annotations.Test;
+
+public class PracticeTest {
+	
+	@Test
+	public void practice()
+	{
+		System.out.println("This is a practice methods ");
+		
+	}
+	
+
+}
